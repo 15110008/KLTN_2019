5d284a024b23a018750e451a0254e01852ec281a
phuc <nguyenngochoangphuc0311@gmail.com>
2019-12-02 20:43:17 +0700

    Update Trip V1

--- Back-end/src/modules/trip-module/repositories/trip.repository.js
+++ Back-end/src/modules/trip-module/repositories/trip.repository.js
@@ -1,21 +1,49 @@
 import TripSchema from '../models/trip.model';
 import TripDetailSchema from '../models/tripDetail.model';
 
+//Trip
 const createTrip = async (data) => {
     const result = await TripSchema.create(data);
     return result;
 };
+const getTripById = async (tripId) => {
+    const result = await TripSchema.findOne({ _id: tripId, isDeleted: false });
+    return result;
+};
+const getTripPublic = async () => {
+    const result = await TripSchema.find({ public: true, isDeleted: false });
+    return result;
+};
+const getTripUnPublic = async (accountId) => {
+    const result = await TripSchema.find({ accountId, isDeleted: false });
+    return result;
+}
+const shareTrip = async (tripId) => {
+    const result = await TripSchema.updateOne({
+        _id: tripId,
+        isDeleted: false
+    },
+    { public: true });
+    return result;
+}
+
+
+//TripDetail
 const createTripDetail = async (data) => {
-    console.log(data);
     const result = await TripDetailSchema.create(data);
     return result;
 };
-const getTripDetail = async () => {
-    const result = await TripDetailSchema.find({ isDeleted: false });
+const getTripDetail = async (tripId) => {
+    const result = await TripDetailSchema.find({ tripId, isDeleted: false });
     return result;
 };
+
 export default {
     createTrip,
+    getTripById,
+    getTripPublic,
+    getTripUnPublic,
+    shareTrip,
     createTripDetail,
     getTripDetail
 };
--- Back-end/src/routes/trip.routes.js
+++ Back-end/src/routes/trip.routes.js
@@ -4,11 +4,28 @@ import TripController from '../modules/trip-module/controllers/trip.controller';
 
 const router = express.Router();
 
-router.post('/trip', TripValidate.createTripInput, TripController.createTrip);
 
+//POST
+//Tạo trip, một trip có nhiều trip detail
+router.post('/trip', TripValidate.createTripInput, TripController.createTrip);
+//Tạo tripDetail cho trip
 router.post('/tripDetail', TripController.createTripDetail);
 
-router.get('/tripDetail', TripController.getTripDetail);
+
+//GET
+//Lấy danh sách trip đã được public hiển thị cho người dùng
+router.get('/trip/Public', TripController.getTripPublic);
+//Lấy danh sách trip của cá nhân người dùng đã tạo
+router.get('/trip/UnPublic', TripController.getTripUnPublic);
+//Xem thông tin chi tiết của trip (gồm trip và tripDetail)
+router.get('/trip/:id', TripController.getTripDetail);
+
+//PUT
+//Người dùng share lịch trình mình đã tạo
+router.put('/trip/:id', TripController.shareTrip);
+
+
+//DELETE
 
 
 export default router;
